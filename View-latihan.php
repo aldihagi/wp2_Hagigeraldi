3029c1e2acdd3c014195983891a893fd3172d30d
aldihagi <90632003+aldihagi@users.noreply.github.com>
2021-09-25 23:58:46 +0700

    Menambahkan Tugas Pertemuan 2

---
+++ View-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web progamming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
