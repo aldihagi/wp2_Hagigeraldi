d1c9127b9351e8ceade168d8e4e691b0855d3212
aldihagi <90632003+aldihagi@users.noreply.github.com>
2021-09-26 00:00:22 +0700

    Update View-latihan.php

--- View-latihan.php
+++ View-latihan.php
@@ -5,7 +5,7 @@
 </head>
 
 <body>
-    Halo Kawan.. Yuk kita belajar web progamming..!!!<br>
+    Halo Kawan.. Yuk kita belajar Web Progamming..!!!<br>
     Nilai 1 = <?= $nilai1; ?>
     Nilai 2 = <?= $nilai2; ?>
     ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
